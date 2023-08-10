d52e90da2137fdd549f505384003ea153c794409
cvdejan <d_cvetkovski@yahoo.com>
2023-08-10 21:45:29 +1000

    Observability

---
+++ src/main/java/io/dejan/configservice/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package io.dejan.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
